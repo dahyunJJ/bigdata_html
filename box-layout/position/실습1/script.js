ab35fefa5739018b98e4c843273e144e40c00e30
dahyunJJ <jeongdh1212@gmail.com>
2024-05-31 11:07:29 +0900

    ✨ open, close sidebar funciton

--- box-layout/position/실습1/script.js
+++ box-layout/position/실습1/script.js
@@ -16,10 +16,24 @@ let sidebar = document.querySelector(".sidebar");
 let sidebarOpenBtn = document.querySelector(".sidebar-open-btn");
 let sidebarCloseBtn = document.querySelector(".sidebar-close-btn");
 
-sidebarOpenBtn.addEventListener("click", () => {
-  sidebar.classList.add("is-active");
-});
+// sidebarOpenBtn.addEventListener("click", () => {
+//   sidebar.classList.add("is-active");
+// });
 
-sidebarCloseBtn.addEventListener("click", () => {
+// sidebarCloseBtn.addEventListener("click", () => {
+//   sidebar.classList.remove("is-active");
+// });
+
+// 함수 생성 및 호출 방법
+function openSidebar() {
+  // 1. sidebar 라는 클래스에 is-active 클래스 추가
+  sidebar.classList.add("is-active");
+}
+
+function closeSidebar() {
+  // 2. sidebar 라는 클래스에 is-active 클래스 제거
   sidebar.classList.remove("is-active");
-});
+}
+
+sidebarOpenBtn.addEventListener("click", openSidebar);
+sidebarCloseBtn.addEventListener("click", closeSidebar);
