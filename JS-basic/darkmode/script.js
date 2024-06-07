a8cf9f95d588686cd0c3d23937d9ba7502fd0890
dahyunJJ <jeongdh1212@gmail.com>
2024-06-07 11:10:53 +0900

    ✨ count 횟수로 mode 변경

--- JS-basic/darkmode/script.js
+++ JS-basic/darkmode/script.js
@@ -1,23 +1,38 @@
+// 1. btn 요소 안의 텍스트를 가져온다.
+// 2. 텍스트가 Dark일 경우 (조건)
+// 3. btn을 클릭하면 (클릭 이벤트) 텍스트를 'Light'로 바꾼다.
+// 4. header 태그에 dark-mode 클래스 add
+
 let header = document.querySelector("header");
 let logo = document.querySelector(".logo");
 let btn = document.querySelector(".btn");
-let text = document.querySelector(".btn").innerHTML;
-// console.log(text);
 
-// btn.addEventListener("click", () => {
-//   header.classList.add("dark-mode");
-//   btn.innerHTML = `Dark`;
-// });
+// function modeToggle() {
+//   let modeTxt = btn.innerHTML;
+
+//   if (modeTxt == "Dark") {
+//     header.classList.add("dark-mode");
+//     btn.innerHTML = `Light`;
+//   } else {
+//     header.classList.remove("dark-mode");
+//     btn.innerHTML = `Dark`;
+//   }
+// }
+
+// 카운트 횟수로 모드 변경
+
+let count = 0;
 
 function modeToggle() {
-  if (text == "Light") {
-    btn.innerHTML = `Dark`;
-    console.log(btn.innerHTML);
+  count = count + 1; // count += 1;
+  console.log(count);
+
+  if (count % 2 == 1) {
     header.classList.add("dark-mode");
-  } else {
     btn.innerHTML = `Light`;
-    console.log(btn.innerHTML);
+  } else if (count % 2 == 0) {
     header.classList.remove("dark-mode");
+    btn.innerHTML = `Dark`;
   }
 }
 
